39aff038845554005d47d6c80b17bd59cd5797ea
Tyler Bray <broncobray77@gmail.com>
2018-03-12 10:38:12 -0700

    added bee and honeymakerbee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
 var Bee = function() {
+    Grub.call(this);
+    this.age = 5;
+    this.color = 'yellow';
 };
 
+Bee.prototype = Object.create(Grub.prototype);//inhertits methods from grub;
+Bee.prototype.job = 'keep on growing';
+Bee.prototype.constructor = Bee; //sets constructor?
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,13 @@
 var HoneyMakerBee = function() {
+    Bee.call(this);
+    this.age = 10;
+    this.job = "make honey";
+    this.honeyPot = 0;
 };
+
+HoneyMakerBee.prototype = Object.create(Grub.prototype); //inherits grub functions
+HoneyMakerBee.prototype.constructor = HoneyMakerBee; //sets constructor for additional functions
+HoneyMakerBee.prototype.makeHoney = function () { this.honeyPot++ }
+HoneyMakerBee.prototype.giveHoney = function () { this.honeyPot-- }
+
 

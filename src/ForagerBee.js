beba01454249559fc0fd521b9bd09a5867d39267
Tyler Bray <broncobray77@gmail.com>
2018-03-12 10:42:11 -0700

    added forager bee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
 var ForagerBee = function() {
+    Bee.call(this); //inherits properties from bee
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Grub.prototype);//inherits functions from grub
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function(value) {
+    this.treasureChest.push(value);
+}
